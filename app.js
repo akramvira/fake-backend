c6edb01c1fd70551a91d0e2a894ffebc735d4981
akram <akram@jumbula.com>
2019-06-23 02:53:04 +0430

    first upload- login dashboard ok

---
+++ app.js
@@ -0,0 +1,50 @@
+var express = require("express");
+var app = express();
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.get("/api/login", (req, res, next) => {
+    res.json({token : "ADMINTOKENNODEJS"});
+});
+app.post("/api/login", (req, res, next) => {
+
+    if (req.headers.username === "admin" && req.headers.password === "123456")
+    {
+        res.status(200);
+        res.json({token : "TOKEN:"+Math.random()});
+    }
+    else{
+        res.status(500);
+        res.json({error : "نام کاربری یا کلمه عبور "});
+    }
+});
+
+
+app.post("/api/dashboard", (req, res, next) => {
+
+    res.json({
+            cpu: parseInt(Math.random()* 100),
+            ram: parseInt(Math.random()* 100),
+            activecall: parseInt(Math.random()* 100),
+            swap: parseInt(Math.random()* 100),
+
+            activeTrunks: parseInt(Math.random()* 100),
+            activeCalls: parseInt(Math.random()* 100),
+            activeChannels: parseInt(Math.random()* 100),
+            callsCountInQueue: parseInt(Math.random()* 100),
+
+            hard: {
+                capacity: 100,
+                use: parseInt(Math.random()* 100),
+                avilable: parseInt(Math.random()* 100)
+            }
+        }
+    );
+});
